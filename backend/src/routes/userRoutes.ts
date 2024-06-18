5915e1ac09cb9bf721fc1e4e6c5838efb0394cf7
unknown <bhoure21@gmail.com>
2024-06-18 12:13:24 +0530

    completing user routes and controllers

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,12 @@
 import express from 'express';
-import { newUser } from '../controllers/newUser.js';
+import { newUser, getAllUsers, getUser, deleteUser } from '../controllers/User.js';
+import { adminOnly } from '../middlewares/auth.js';
 const router = express.Router();
 
 // base route: /api/v1/user
 router.post('/new', newUser)
+router.get("/all", adminOnly, getAllUsers);
+router.get("/:id", getUser);
+router.delete("/:id", adminOnly, deleteUser)
 
 export default router;
