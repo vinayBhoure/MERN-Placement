645fbe49905835fba71ec81ac9c9f73dd4892257
unknown <bhoure21@gmail.com>
2024-06-18 12:12:02 +0530

    updating global catch, simplifing trycatch functionality, creating errortypes

--- backend/src/middlewares/error.ts
+++ backend/src/middlewares/error.ts
@@ -1,15 +1,25 @@
 import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/class.js";
+import { ControllerType } from "../types/types.js";
 
 export const globalCatch = (
-    err: Error,
+    err: ErrorHandler,
     req: Request,
     res: Response,
     next: NextFunction) => {
 
-    
-        return res.status(500).json({
+        err.message = err.message || "Internal server error";
+        err.statusCode = err.statusCode || 500;
+
+        return res.status(err.statusCode).json({
             success: false,
             error: err.message
         })
 
      }
+
+export const TryCatch = (func: ControllerType) => {
+    return (req:Request, res:Response, next:NextFunction) => {
+        return Promise.resolve(func(req,res,next)).catch(next)
+    }
+}
--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -1,9 +1,17 @@
-export interface NewUserRequestBody{
+import { Request, NextFunction, Response } from "express";
+
+export interface NewUserRequestBody {
     name: string;
     email: string;
     photo: string;
     gender: string;
-    role: string;
+    role?: string;
     _id: string;
     dob: Date;
 }
+
+export type ControllerType = (
+    req: Request,
+    res: Response,
+    next: NextFunction) =>
+    Promise<void | Response<any, Record<string, any>>>
