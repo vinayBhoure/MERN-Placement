3fb0cc1428b3738e66a8b01f1d885801bd327e6f
unknown <bhoure21@gmail.com>
2024-06-22 01:16:53 +0530

    updating auth controller and adding multer middleware for file handling

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -5,12 +5,12 @@ import ErrorHandler from "../utils/class.js";
 export const adminOnly = TryCatch(async (req, res, next) => {
     const { id } = req.query;
 
-    if (!id) return next(new ErrorHandler("Saale Login Kr phle", 401));
+    if (!id) return next(new ErrorHandler("Please Login First", 401));
   
     const user = await User.findById(id);
-    if (!user) return next(new ErrorHandler("Saale Fake ID Deta Hai", 401));
+    if (!user) return next(new ErrorHandler("Invalid user ID", 401));
     if (user.role !== "admin")
-      return next(new ErrorHandler("Saale Aukat Nhi Hai Teri", 403));
+      return next(new ErrorHandler("You are not allowed to access protected routes", 403));
   
     next();
 })
---
+++ backend/src/middlewares/multer.ts
@@ -0,0 +1,18 @@
+import multer from 'multer'
+import {v4 as uuid} from 'uuid'
+
+const storage = multer.diskStorage({
+    destination(req, file, callback){
+           callback(null, "uploads")
+    },
+    filename(req, file, callback){
+
+       const id = uuid();
+       const extensionName = file.originalname.split('.').pop();
+       const filename = `${id}.${extensionName}`
+
+           callback(null, filename);
+    }
+});
+
+export const fileUpload = multer({storage}).single("photo")
