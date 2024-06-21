083670540168c2ed9aa58c34c93a79d5727a55b0
unknown <bhoure21@gmail.com>
2024-06-22 01:22:45 +0530

    updating main server file for handling routes and middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,43 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import paymentRoutes from './routes/paymentRoutes.js';
+import statsRoutes from './routes/statsRoutes.js';
 import { connectDB } from './config/connectDB.js';
 import { globalCatch } from './middlewares/error.js';
+import NodeCache from 'node-cache';
+import { config } from 'dotenv';
+import morgan from 'morgan';
+import Stripe from 'stripe';
 const app = express();
 const PORT = 3000;
+
+export const stripe = new Stripe("dsafa");
+export const nodeCache = new NodeCache();
+config({
+    path: "./.env"
+})
+
+// creating a static folder - uploads. Without this middlerware, it will be treated as apiroute.
+app.use("/uploads", express.static("uploads")); 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get('/', (req, res) => {
- res.send('Express + TypeScript Server');
+    res.send('Express + TypeScript Server');
 })
 
 app.use('/api/v1/user', userRoutes);
+app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/order", orderRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+app.use("/api/v1/dashboard", statsRoutes);
 
 app.use(globalCatch);
 
 app.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 })
 
 connectDB();
