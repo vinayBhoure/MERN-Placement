c90bf0542eec49e0b44479c52fdf5a1b4642c0bc
unknown <bhoure21@gmail.com>
2024-06-22 01:15:59 +0530

    defining types for request, query, controller and props

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -1,17 +1,78 @@
 import { Request, NextFunction, Response } from "express";
 
 export interface NewUserRequestBody {
-    name: string;
-    email: string;
-    photo: string;
-    gender: string;
-    role?: string;
-    _id: string;
-    dob: Date;
+  name: string;
+  email: string;
+  photo: string;
+  gender: string;
+  _id: string;
+  dob: Date;
+}
+
+export interface NewProductRequestBody {
+  name: string;
+  category: string;
+  price: number;
+  stock: number;
+}
+
+export type OrderItemType = {
+  name: string;
+  photo: string;
+  price: number;
+  quantity: number;
+  productId: string;
+};
+
+export type ShippingInfoType = {
+  address: string;
+  city: string;
+  state: string;
+  country: string;
+  pinCode: number;
+};
+
+export interface NewOrderRequestBody {
+  shippingInfo: ShippingInfoType;
+  user: string;
+  subtotal: number;
+  tax: number;
+  shippingCharges: number;
+  discount: number;
+  total: number;
+  orderItems: OrderItemType[];
 }
 
 export type ControllerType = (
-    req: Request,
-    res: Response,
-    next: NextFunction) =>
-    Promise<void | Response<any, Record<string, any>>>
+  req: Request,
+  res: Response,
+  next: NextFunction) =>
+  Promise<void | Response<any, Record<string, any>>>
+
+export type SearchRequestQuery = {
+  category?: string;
+  price?: string;
+  sort?: string;
+  page?: string;
+  search?: string;
+}
+
+export interface BaseQueryForSearch {
+  name?: {
+    $regex: string;
+    $options: string;
+  };
+  price?: {
+    $lte: number;
+  };
+  category?: string;
+}
+
+export type InvalidateCacheProp = {
+  product?: boolean;
+  order?: boolean;
+  admin?: boolean;
+  userId?: string;
+  orderId?: string;
+  productId?: string | string[];
+}
